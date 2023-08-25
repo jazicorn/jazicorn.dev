84c9407e3f350c2c1d48ad029d10abcc78d8278b
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-08-25 17:51:22 -0400

    changed background color

--- apps/web/src/components/Home.tsx
+++ apps/web/src/components/Home.tsx
@@ -14,7 +14,7 @@ import useWindowSize from '../hooks/useWindowSize';
 import Transition from '../hooks/useTransition';
 
 const Home = () => {
-  const { isMobile, isDesktopMDLG, isDesktopXL } = useWindowSize();
+  const { isMobile, isDesktopMDXL, isDesktopXL } = useWindowSize();
   const theme = useContext(ThemeContext)
   const darkMode = theme.state.darkMode
 
@@ -22,14 +22,16 @@ const Home = () => {
     <Transition>
       <main className={`${darkMode ? 'tw-text-white' : 'tw-text-black'}
                         ${isMobile && 'tw-grid-cols-layout-home-mobile tw-grid-rows-layout-home-mobile'} 
-                        ${isDesktopMDLG && 'tw-grid-cols-layout-home tw-grid-rows-layout-home'} 
+                        ${isDesktopMDXL && 'tw-grid-cols-layout-home tw-grid-rows-layout-home'} 
                         ${isDesktopXL && 'tw-grid-cols-layout-home-xl tw-grid-rows-layout-home-xl'} 
-                        tw-text-center [&>*]:tw-border-jazicorn-pink tw-h-screen min-h-screen tw-w-full tw-grid `}>
-        <article className={`${isMobile ? 'tw-border-t': 'tw-border-x'} tw-h-full tw-w-full tw-flex tw-flex-col tw-place-items-center tw-place-content-center
-                            tw-col-start-2 tw-col-end-2 tw-row-span-1  tw-relative`}>
+                        tw-text-center [&>*]:tw-border-jazicorn-pink tw-h-full tw-w-full tw-grid`}>
+        <article className={`${isMobile ? 'tw-border-t': 'tw-border-x'} tw-h-full tw-w-full 
+                              tw-flex tw-flex-col tw-place-items-center tw-place-content-center 
+                              tw-col-start-2 tw-col-end-2 tw-row-span-1  tw-relative `}>
             {/**Backdrop */}
-            <div className={`${darkMode ? "tw-bg-jazicorn-gray-400 [&>*]:tw-border-gray-300" : "tw-bg-gray-100 [&>*]:tw-border-jazicorn-pink-300"} 
-            tw-grid tw-grid-cols-layout-banner tw-grid-rows-layout-banner tw-relative tw-h-full tw-w-full `}>
+            <div className={`${darkMode ? "tw-bg-neutral-800 [&>*]:tw-border-gray-300" 
+                              : "tw-bg-gray-50 [&>*]:tw-border-jazicorn-pink-300"} tw-relative
+                              tw-grid tw-grid-cols-layout-banner tw-grid-rows-layout-banner tw-h-full tw-w-full`}>
               <div className={`${darkMode ? "" : ""} 
                             tw-col-start-3 tw-col-end-7 tw-row-span-1 tw-rounded-bl-2xl tw-border-l-2 tw-border-b-2 tw-border-dotted`}/>
               <div className={`${darkMode ? "" : ""} 
