78458313987c03cff58a774ba459d00e6b213ff7
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-08-25 17:46:46 -0400

    created seperate files for header mobile and desktop

---
+++ apps/web/src/components/header/Header.tsx
@@ -0,0 +1,42 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+// hooks
+import useWindowSize from '../../hooks/useWindowSize';
+import { ThemeContext } from '../../context/ThemeContext';
+// icons
+import { 
+    IconHome,
+    IconSun,
+    IconMoon,
+    IconMail,
+    IconBrandGithub,
+    IconBrandLinkedin 
+} from '@tabler/icons-react';
+import HeaderDesktop from './HeaderDesktop';
+import HeaderMobile from './HeaderMobile';
+
+
+const Header = () => {
+const { isMobile, isMobileMD, isDesktopMDLG, isDesktopLG, isDesktopXL } = useWindowSize();
+  const theme = useContext(ThemeContext)
+  const darkMode = theme.state.darkMode
+  //turn darkmode on and off
+  const toggleTheme = () => {
+    if (darkMode) {
+      theme.dispatch({ type: 'LIGHTMODE', darkMode: false })
+      localStorage.theme = 'light'
+    } else {
+      theme.dispatch({ type: 'DARKMODE', darkMode: true })
+      localStorage.theme = 'dark'
+    }
+  }
+
+  return (
+    <div className={`${darkMode ? 'tw-bg-black tw-text-jazicorn-gray ' : 'tw-bg-light'}`}>
+        {isMobileMD && <HeaderMobile/>}
+        {isDesktopLG && <HeaderDesktop/>}
+    </div>
+  )
+}
+
+export default Header
