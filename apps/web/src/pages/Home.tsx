4b980aab34075fe9f1eec6cd6272f2448cf570c9
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-08-30 00:26:40 -0400

    update style

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -22,7 +22,7 @@ const Home = () => {
     <Transition>
       <main className={`${darkMode ? 'tw-text-white' : 'tw-text-black'}
                         ${isMobile && 'tw-grid-cols-layout-home-mobile tw-grid-rows-layout-home-mobile'} 
-                        ${isDesktopMDXL && 'tw-grid-cols-layout-home tw-grid-rows-layout-home'} 
+                        ${isDesktopMDXL && 'tw-grid-cols-layout-home-xl tw-grid-rows-layout-home-xl'} 
                         ${isDesktopXL && 'tw-grid-cols-layout-home-xl tw-grid-rows-layout-home-xl'} 
                         tw-text-center [&>*]:tw-border-jazicorn-pink tw-h-full tw-w-full tw-grid`}>
         <article className={`${isMobile ? 'tw-border-t': 'tw-border-x'} tw-h-full tw-w-full 
@@ -42,13 +42,13 @@ const Home = () => {
                           tw-col-start-5 tw-col-end-6 tw-row-start-5 tw-row-end-5 tw-border-x-2 tw-border-t-2 tw-rounded-t-2xl tw-border-dotted `}/>
             </div>
             {/**Information */}
-            <section className={ `${darkMode ? "tw-bg-neutral-700 tw-border-gray-300" : "tw-bg-gray-200 tw-border-jazicorn-pink-200"} tw-w-full tw-py-16 tw-border tw-z-100 tw-absolute tw-place-items-center tw-items-center`}>
+            <section className={ `${darkMode ? "tw-bg-neutral-700 tw-border-gray-300" : "tw-bg-gray-100 tw-border-jazicorn-pink-200"} tw-w-full tw-py-16 tw-border-y tw-z-100 tw-absolute tw-place-items-center tw-items-center`}>
               <div className={`${darkMode ? "" : ""}`}>
                 <h1 className={`${isMobile && 'h8 tw-leading-3'} ${isDesktopMDXL && 'h7 tw-leading-6'} 
                 ${isDesktopXL && 'h5 tw-leading-8'}
                 tw-text-jazicorn-pink`}>Jasmine Anderson</h1>
               </div>
-              <div className={`${isMobile && 'tw-text-sm tw-w-fit'} ${isDesktopMDXL && 'tw-text-base'} ${isDesktopXL && 'tw-text-lg'}  tw-mt-8`}>
+              <div className={`${isMobile && 'tw-text-sm '} ${isDesktopMDXL && 'tw-text-base'} ${isDesktopXL && 'tw-text-lg'}  tw-mt-8`}>
                 <p>Software Engineer // Web Developer // Javascript Developer</p>
               </div>
             </section>
